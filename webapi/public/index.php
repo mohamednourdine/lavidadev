a709a1460a4b67f64f4f46942d8036ccc8a74ea5
Trey <trey@lavidadev.com>
2019-04-28 18:17:29 -0400

    Remove test script

--- webapi/public/index.php
+++ webapi/public/index.php
@@ -1,18 +1,4 @@
 <?php
-//$servername = "db";
-//$username = "site";
-//$password = "secret";
-//
-//try {
-//    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-//    // set the PDO error mode to exception
-//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    echo "Connected successfully";
-//}
-//catch(PDOException $e)
-//{
-//    echo "Connection failed: " . $e->getMessage();
-//}
 
 $app = require __DIR__ . '/../src/web.php';
 
