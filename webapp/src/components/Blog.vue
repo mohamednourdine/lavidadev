6135ef3c8b0d045a5da6cb867511e3b66805d76a
Trey <trey@lavidadev.com>
2019-06-16 20:08:24 -0400

    Add Axios dependency

--- webapp/src/components/Blog.vue
+++ webapp/src/components/Blog.vue
@@ -34,8 +34,6 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   data () {
     return {
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 new Vue({
   router,
   store,
